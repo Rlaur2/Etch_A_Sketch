003b32e3212df750f9a4d866fd906778fb1da462
pavel <plaureanopena@gmail.com>
2022-03-15 18:00:32 -0400

    Add EaS functionality and Reset button CSS

--- etch.js
+++ etch.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
-for (i = 0; i < 255; i++) {
+for (i = 0; i < 256; i++) {
     square = document.createElement('div');
     square.classList.toggle('square');
     container.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+
+function hover (e) {
+    this.classList.add('hovered');
+}
+
+for (square of squares) {
+    square.addEventListener('mouseover',hover);
+}
+
