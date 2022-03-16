f6073816d58ed2080f9aa6adeda9b73d5091bc80
pavel <plaureanopena@gmail.com>
2022-03-16 02:48:49 -0400

    Finish barebones version

--- etch.js
+++ etch.js
@@ -1,18 +1,44 @@
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('.button');
 
-for (i = 0; i < 256; i++) {
+//loop to create inital amount of 16x16 squares
+const gridCreation = (grid = 16) => {
+for (i = 0; i < grid * grid; i++) {
     square = document.createElement('div');
     square.classList.toggle('square');
+    squareSize = 900 / grid;
+    square.style.cssText = `height: ${squareSize}px; width: ${squareSize}px;`;
     container.appendChild(square);
+    //find out out to use the forEach method, I only know how to work a 'for of' loop
 }
+squares = document.querySelectorAll('.square');
+for (square of squares) {
+    square.addEventListener('mouseover',hover);
+}}
 
-const squares = document.querySelectorAll('.square');
+gridCreation();
 
+//function to change color of squares when moused over
 function hover (e) {
     this.classList.add('hovered');
 }
 
+//function for reset grid button, had to look up the 'isNaN' method
+const resetGrid = () => {
+    newGrid = Number((prompt('How many squares per side would you like? Choose a number between (and including) 1 and 100.')));
+    if (newGrid > 100 || newGrid < 1 || isNaN(newGrid)) {
+        alert('Please enter a number between (and including) 1 and 100!');
+        return;
+    }
+    for (square of squares) {
+        container.removeChild(square);
+    }
+    gridCreation(newGrid);
+}
+
+//find out out to use the forEach method, I only know how to work a 'for of' loop
 for (square of squares) {
     square.addEventListener('mouseover',hover);
 }
 
+resetButton.addEventListener('click',resetGrid);
