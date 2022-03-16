9e11bf0167c52c9106b2757aedea48196bbdcb22
pavel <plaureanopena@gmail.com>
2022-03-16 15:55:31 -0400

    Add draw and erase button with completed functionality

--- etch.js
+++ etch.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.button');
+const drawButton = document.querySelector('#draw');
+const eraseButton = document.querySelector('.erase');
 
 //loop to create inital amount of 16x16 squares
 const gridCreation = (grid = 16) => {
@@ -12,15 +14,18 @@ for (i = 0; i < grid * grid; i++) {
     //find out out to use the forEach method, I only know how to work a 'for of' loop
 }
 squares = document.querySelectorAll('.square');
-for (square of squares) {
-    square.addEventListener('mouseover',hover);
-}}
+}
 
 gridCreation();
 
-//function to change color of squares when moused over
-function hover (e) {
+//function to add color of squares when moused over
+function hoverAdd (e) {
     this.classList.add('hovered');
+}
+
+//function to delete color of squares when moused over
+function hoverDelete (e) {
+    this.classList.remove('hovered');
 }
 
 //function for reset grid button, had to look up the 'isNaN' method
@@ -33,7 +38,31 @@ const resetGrid = () => {
     for (square of squares) {
         container.removeChild(square);
     }
+    drawButton.classList.remove('draw-clicked');
+    eraseButton.style.cssText = '';
     gridCreation(newGrid);
 }
 
 resetButton.addEventListener('click',resetGrid);
+
+//function to activate draw capability and remove erase capability
+const draw = () => {
+    drawButton.classList.add('draw-clicked');
+    eraseButton.style.cssText = '';
+    for (square of squares) {
+        square.addEventListener('mouseover',hoverAdd);
+        square.removeEventListener('mouseover',hoverDelete);
+    }
+};
+drawButton.addEventListener('click',draw);
+
+//counter function that erases and removes draw ability
+const erase = () => {
+    eraseButton.style.cssText = 'background-color: red; color: black; outline: 2px black solid;'
+    drawButton.classList.remove('draw-clicked');
+    for (square of squares) {
+        square.addEventListener('mouseover',hoverDelete);
+        square.removeEventListener('mouseover',hoverAdd);        
+    }
+};
+eraseButton.addEventListener('click',erase);
