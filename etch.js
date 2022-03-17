59154dcf593953ed50057c6cc69f5db90585d1a2
pavel <plaureanopena@gmail.com>
2022-03-16 23:03:59 -0400

    Add toggle functionality to buttons and update comments

--- etch.js
+++ etch.js
@@ -4,16 +4,25 @@ const drawButton = document.querySelector('#draw');
 const eraseButton = document.querySelector('.erase');
 
 //function to activate draw capability and remove erase capability
+//can be toggled
 const draw = () => {
+    if (drawButton.className === 'smButton draw-clicked') {
+        drawButton.classList.remove('draw-clicked');
+        for (square of squares) {
+            square.removeEventListener('mouseover',hoverAdd);
+        }
+    } else {
     drawButton.classList.add('draw-clicked');
     eraseButton.style.cssText = '';
     for (square of squares) {
         square.addEventListener('mouseover',hoverAdd);
         square.removeEventListener('mouseover',hoverDelete);
-    }
+        }
+    }  
 };
 
-//loop to create inital amount of 16x16 squares
+//loop to create grid based off the number given, default is 16x16
+//also turns on the Draw function
 const gridCreation = (grid = 16) => {
 for (i = 0; i < grid * grid; i++) {
     square = document.createElement('div');
@@ -22,12 +31,12 @@ for (i = 0; i < grid * grid; i++) {
     squareHeight = container.offsetHeight / grid;
     square.style.cssText = `height: ${squareHeight}px; width: ${squareWidth}px;`;
     container.appendChild(square);
-    //find out out to use the forEach method, I only know how to work a 'for of' loop
 }
 squares = document.querySelectorAll('.square');
 draw();
 }
 
+//creates initial default grid
 gridCreation();
 
 //function to add color of squares when moused over
@@ -40,7 +49,7 @@ function hoverDelete (e) {
     this.classList.remove('hovered');
 }
 
-//function for reset grid button, had to look up the 'isNaN' method
+//function for reset grid button, had to look up the 'isNaN' method, newGrid !== 'number' doesn't work
 const resetGrid = () => {
     newGrid = Number((prompt('How many squares per side would you like? Choose a number between (and including) 1 and 100.')));
     if (newGrid > 100 || newGrid < 1 || isNaN(newGrid)) {
@@ -50,22 +59,27 @@ const resetGrid = () => {
     for (square of squares) {
         container.removeChild(square);
     }
-    drawButton.classList.remove('draw-clicked');
-    eraseButton.style.cssText = '';
     gridCreation(newGrid);
 }
 
-resetButton.addEventListener('click',resetGrid);
-
-drawButton.addEventListener('click',draw);
-
-//counter function that erases and removes draw ability
+//counter function that erases and removes draw ability, can be toggled
 const erase = () => {
-    eraseButton.style.cssText = 'background-color: red; color: black; outline: 2px black solid;'
+    if (eraseButton.style.cssText === 'background-color: red; color: black; outline: black solid 2px;') {
+        eraseButton.style.cssText = '';
+        for (square of squares) {
+            square.removeEventListener('mouseover',hoverDelete);
+        }
+    } else {
+    eraseButton.style.cssText = 'background-color: red; color: black; outline: 2px black solid;';
     drawButton.classList.remove('draw-clicked');
     for (square of squares) {
         square.addEventListener('mouseover',hoverDelete);
         square.removeEventListener('mouseover',hoverAdd);        
+      }
     }
 };
+
+//Event listeners for the three buttons
+resetButton.addEventListener('click',resetGrid);
+drawButton.addEventListener('click',draw);
 eraseButton.addEventListener('click',erase);
